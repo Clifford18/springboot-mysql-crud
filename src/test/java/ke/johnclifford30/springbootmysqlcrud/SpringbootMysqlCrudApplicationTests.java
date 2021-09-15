6359e2d5a8a76e365bb091d7c28b7d858088379a
Clifford18 <cliffordjohn.cj@gmail.com>
2021-09-15 15:04:51 +0300

    adding maven project

---
+++ src/test/java/ke/johnclifford30/springbootmysqlcrud/SpringbootMysqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.johnclifford30.springbootmysqlcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
