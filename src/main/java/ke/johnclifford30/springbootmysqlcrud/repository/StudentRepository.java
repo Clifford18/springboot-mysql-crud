799de8895ac974f83d886910478f816352bd93f0
Clifford18 <cliffordjohn.cj@gmail.com>
2021-09-21 13:06:28 +0300

    update StudentRepository.java with imports

--- src/main/java/ke/johnclifford30/springbootmysqlcrud/repository/StudentRepository.java
+++ src/main/java/ke/johnclifford30/springbootmysqlcrud/repository/StudentRepository.java
@@ -1,8 +1,9 @@
 package ke.johnclifford30.springbootmysqlcrud.repository;
 
+import ke.johnclifford30.springbootmysqlcrud.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository <Student, Long {
+public interface StudentRepository extends JpaRepository <Student, Long> {
 }
